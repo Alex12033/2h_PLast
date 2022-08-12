e285afe70e709b27f011cfb8139cde69bb66decb
Alex <alex.fursal_m@outlook.com>
2022-08-12 09:35:27 +0300

    add strings require timer and modal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 let component = require('./components/component');
 let burgerMenu = require('./components/burger_menu');
-
-component();
-burgerMenu();
 let timer = require('./components/modal-window/timer');
 let modal = require('./components/modal-window/modal');
 
-modal();
-timer();
 component();
+burgerMenu();
+timer();
+modal();
+
