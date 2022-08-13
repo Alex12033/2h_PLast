f3069cfc253a32e489205bdcd440a33de34ee3ef
Aleksandr <aleksandrignashov42@gmail.com>
2022-08-14 01:09:29 +0300

    create main service section

--- src/components/component.js
+++ src/components/component.js
@@ -1,6 +1,8 @@
 require("../scss/index.scss");
 require("../scss/header.scss");
 require("../scss/experience.scss");
+require("../scss/main-services.scss");
+
 
 function hello() {
     console.log("script works fine");
