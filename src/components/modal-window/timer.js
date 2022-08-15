b1f24238da62a1f4af87e77564cd5ad725e36ce9
Jamil <104577469+JamXDev@users.noreply.github.com>
2022-08-15 09:59:33 +0300

    finaly version create modal

--- src/components/modal-window/timer.js
+++ src/components/modal-window/timer.js
@@ -1,56 +1,29 @@
-require('../../scss/modal/timer.scss');
+require("../../scss/modal/timer.scss");
+
+let date = new Date("Aug 15 2022 09:55:00");
 
 function timer() {
-  const deadLine = "2022-09-10";
+  let now = new Date();
+  gap = date - now;
 
-  function getTimeRemaning(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date());
-    const days = Math.floor(t / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((t / 1000 / 60) % 60);
-    const seconds = Math.floor((t / 1000) % 60);
+  let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+  let minutes = Math.floor(gap / 1000 / 60) % 60;
+  let seconds = Math.floor(gap / 1000) % 60;
 
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-      total: t,
-    };
+  if (gap < 0) {
+    days = days + 7;
+    hours = hours + 24;
+    minutes = minutes + 60;
+    seconds = seconds + 60;
   }
 
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
-
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector("#days"),
-      hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updateClock, 1000);
-
-    updateClock();
-
-    function updateClock() {
-      const t = getTimeRemaning(endtime);
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
-
-      if (t.total <= 0) {
-        clearInterval(timeInterval);
-      }
-    }
-  }
-
-  setClock(".timer", deadLine);
+  document.getElementById("d").innerText = days;
+  document.getElementById("h").innerText = hours;
+  document.getElementById("m").innerText = minutes;
+  document.getElementById("s").innerText = seconds;
 }
+timer();
+setInterval(timer, 1000);
 
 module.exports = timer;
