26d800d9fcbdc2577d208ac727485fba2cbdd3f2
Jamil <104577469+JamXDev@users.noreply.github.com>
2022-08-15 11:48:53 +0300

    create footer

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -3,6 +3,7 @@ require("../scss/header.scss");
 require("../scss/experience.scss");
 require("../scss/main-services.scss");
 require("../scss/our-services.scss");
+require("../scss/footer.scss");
 
 function tabs() {
   let tabs = document.querySelector(".tabs-variants"); //btns for change tab
