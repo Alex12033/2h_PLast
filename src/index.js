5d8f843eae7f23820ff06eff69d7f6f737f031af
Alex <alex.fursal_m@outlook.com>
2022-08-16 13:41:44 +0300

    add slider (reviews)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 let component = require('./components/tabs');
 let burgerMenu = require('./components/burger_menu');
 let timer = require('./components/modal-window/timer');
-// let modal = require('./components/modal-window/modal');
+let modal = require('./components/modal-window/modal');
 let slider = require('./components/slider');
 
 
 component();
 burgerMenu();
 timer();
-// modal();
+modal();
 slider();
