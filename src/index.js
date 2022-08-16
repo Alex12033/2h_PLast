01cded65ac3aa2bea7b171902c176cb6dbd59ef7
Aleksandr <aleksandrignashov42@gmail.com>
2022-08-16 12:07:46 +0300

    adaptate tab section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 let component = require('./components/tabs');
 let burgerMenu = require('./components/burger_menu');
-let timer = require('./components/modal-window/timer');
-let modal = require('./components/modal-window/modal');
+// let timer = require('./components/modal-window/timer');
+// let modal = require('./components/modal-window/modal');
 
 
 component();
 burgerMenu();
-timer();
-modal();
+// timer();
+// modal();
