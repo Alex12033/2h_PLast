802b1998b534535e150dfeab4b97ff11d850c028
Aleksandr <aleksandrignashov42@gmail.com>
2022-09-26 20:47:16 +0300

    fix and upgrade slider on main page, fix when timer ends value not go in negative numbers

--- src/components/burger_menu.js
+++ src/components/burger_menu.js
@@ -6,19 +6,25 @@ function burgerMenu() {
   const closedMobileNav = document.querySelector('.mobile-nav__close');
   const burgerBackdrop = document.querySelector('.mobile-nav__backdrop');
 
+  function closeMenu(selector) {
+    selector.addEventListener('click', () => {
+      mobileNav.classList.add('hide-back');
+      setTimeout(() => {
+        mobileNav.classList.remove('hide-back');
+        mobileNav.style.display = 'none';
+        burgerBackdrop.style.display = 'none';
+      }, 1000);
+    });
+  }
+
   burgerBtn.addEventListener('click', () => {
     mobileNav.style.display = 'flex';
     burgerBackdrop.style.display = 'block';
   });
 
-  closedMobileNav.addEventListener('click', () => {
-    mobileNav.classList.add('hide-back');
-    setTimeout(() => {
-      mobileNav.classList.remove('hide-back');
-      mobileNav.style.display = 'none';
-      burgerBackdrop.style.display = 'none';
-    }, 1000);
-  });
+  closeMenu(closedMobileNav);
+
+  closeMenu(burgerBackdrop);
 }
 
 module.exports = burgerMenu;
--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
-    clean: true,
+    // clean: true,
   },
 
   devServer: {
